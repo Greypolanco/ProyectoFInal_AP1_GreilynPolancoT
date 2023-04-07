95b865a6813d160a18eaa6e62f58fbeac8f6fe01
Greilyn Polanco <greilyntejada03@gmail.com>
2023-04-07 17:07:28 -0400

    modificados

--- Models/Compras.cs
+++ Models/Compras.cs
@@ -7,8 +7,6 @@ public class Compras
     public string Concepto { get; set; } = string.Empty;
     [ForeignKey("CompraId")]
     public virtual List<CompraDetalles> CompraDetalles { get; set; } = new List<CompraDetalles>();
-    public int Cantidad { get; set; }
-    public int ProductoId { get; set; }
     public int ProveedorId { get; set; }
     
 }
