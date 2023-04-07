5e266e1723a259107460f82d8b8d43ffd9faf803
Greilyn Polanco <greilyntejada03@gmail.com>
2023-04-07 13:27:38 -0400

    Migracion y base de datos

--- Data/Migrations/20230407170600_primera_migracion.Designer.cs
+++ Data/Migrations/20230407170600_primera_migracion.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProyectoFinal_AP1_GreilynPolancoT.Data;
 
@@ -10,9 +11,11 @@ using ProyectoFinal_AP1_GreilynPolancoT.Data;
 namespace ProyectoFinalAP1GreilynPolancoT.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230407170600_primera_migracion")]
+    partial class primeramigracion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.2");
