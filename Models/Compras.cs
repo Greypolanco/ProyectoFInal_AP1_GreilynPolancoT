c52918c4621f56b8d5a6b5f686427707e9dd4774
Greilyn Polanco <greilyntejada03@gmail.com>
2023-04-08 21:37:00 -0400

    actualizada

--- Models/Compras.cs
+++ Models/Compras.cs
@@ -3,7 +3,7 @@ public class Compras
     [Key]
     public int CompraId { get; set; }
     public DateTime Fecha { get; set; } = DateTime.Today;
-    
+    [Required (ErrorMessage ="El concepto de la compra requerido")]
     public string Concepto { get; set; } = string.Empty;
     [ForeignKey("CompraId")]
     public virtual List<CompraDetalles> CompraDetalles { get; set; } = new List<CompraDetalles>();
